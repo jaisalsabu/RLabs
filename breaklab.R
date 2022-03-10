5168efcfbfa774e773be8f602e529189d030b296
jaisalsabu <jaisal1010sabu@gmail.com>
2022-03-10 10:54:27 +0000

    beforebreak

---
+++ breaklab.R
@@ -0,0 +1,14 @@
+5 + 9 
+x <- 5+3
+print(x)
+
+z <- c(1, 1, 3, 4.56)
+z <- c(z, 344, z)
+z
+z <- z * 2 + 100
+z
+my_sqrt <- sqrt(z-1)
+my_sqrt
+my_data <- c(1, 2,33) + c(0, 34)
+my_data
+print(my_data)
