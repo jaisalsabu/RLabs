29b2079dbbaeb9125e99886dabc73604afc1cd55
jaisalsabu <jaisal1010sabu@gmail.com>
2022-03-10 12:29:34 +0000

    lab on thursday complete

--- breaklab.R
+++ breaklab.R
@@ -7,8 +7,24 @@ z <- c(z, 344, z)
 z
 z <- z * 2 + 100
 z
-my_sqrt <- sqrt(z-1)
-my_sqrt
-my_data <- c(1, 2,33) + c(0, 34)
-my_data
-print(my_data)
+pi:20
+15:1
+seq(1 , 20)
+seq(0 , 10, 0.2)
+seq(5, 20, length= 30)
+my_seq <- seq(5,10,length=30)
+length(my_seq)
+
+my_var <- c(0, 1, 3)
+rep(my_var , times=40)
+rep(my_var , each=40)
+num_vect <- c(0.5, 55 , -10, 6)
+tf <- num_vect < 1
+tf 
+rf <- num_vect>=6
+rf
+my_char <- c("My", "Name", "is")
+paste(my_char ,collapse = " ")
+my_char <- c(my_char,"jaisal")
+my_char
+paste(my_char ,collapse = " ")
